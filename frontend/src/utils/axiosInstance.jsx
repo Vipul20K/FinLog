7be75942d8aa559d455dcb85c82ee01904209cbf
Vipul Kumar <143587680+Vipul20K@users.noreply.github.com>
2025-08-17 11:24:04 +0530

    Update axiosInstance.jsx

--- frontend/src/utils/axiosInstance.jsx
+++ frontend/src/utils/axiosInstance.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 import Cookies from "js-cookie";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:5000/api/v1",
+  baseURL: import.meta.env.VITE_API_BASE_URL,
 });
 
 // Attach token from cookie into Authorization header
